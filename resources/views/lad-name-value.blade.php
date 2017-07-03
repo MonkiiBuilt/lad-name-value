3690461e5e5d97261e86f9c9ac21e99c50ceda44
Chris Campbell <chris@monkii.com>
2017-07-04 08:36:47 +1000

    Refactored markup to match design requirments, couldnt get JS working.

--- resources/views/lad-name-value.blade.php
+++ resources/views/lad-name-value.blade.php
@@ -7,58 +7,83 @@
  */
 ?>
 
-<div class="highlights">
+    <div class="highlights">
 
-    <h3>Highlights</h3>
+        <h3>Highlights</h3>
 
-    <div class="highlights__list">
-        <!-- Highlight block START -->
-        <div class="highlight  panel  panel-default">
-            <div class="panel-body">
+        <div class="highlights__list">
 
-                <button type="button" class="close  pull-right" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <!-- Repeatable Highlight block START -->
+            <div class="highlight  panel  panel-default">
+                <div class="panel-body">
 
-                <h4>Highlight</h4>
+                    <button type="button" class="close  pull-right" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 
-                <div class="form-group  clearfix">
-                    <fieldset class="{{ $errors->has('sections.' . $section->id . '.data.0.description') ? 'error' : '' }}">
-                        <label for="highlight-description-{{ $section->id }}">Description</label>
-                        {!! Form::text( 'sections[' . $section->id . '][data][0][description]', $section->data[0]['description'], array('id' => 'highlight-description-' . $section->id, 'class' => 'form-control')) !!}
-                        <div class="form__error">{{ $errors->first('sections.' . $section->id . '.data.0.description') }}</div>
-                    </fieldset>
-                </div>
+                    <h4>Highlight</h4>
 
-                <div class="form-group">
-                    <fieldset class="{{ $errors->has('sections.' . $section->id . '.data.0.value') ? 'error' : '' }}">
-                        <label for="highlight-value-{{ $section->id }}">Value</label>
-                        {!! Form::text( 'sections[' . $section->id . '][data][0][value]', $section->data[0]['value'], array('id' => 'highlight-value-' . $section->id, 'class' => 'form-control')) !!}
-                        <div class="form__error">{{ $errors->first('sections.' . $section->id . '.data.0.value') }}</div>
-                    </fieldset>
-                </div>
+                    <!-- Description -->
+                    <div class="form-group  clearfix">
+                        <fieldset class="{{ $errors->has('sections.' . $section->id . '.data.0.description') ? 'error' : '' }}">
+                            <label for="highlight-description-{{ $section->id }}">Description</label>
+                            {!! Form::text( 'sections[' . $section->id . '][data][0][description]', $section->data[0]['description'], array('id' => 'highlight-description-' . $section->id, 'class' => 'form-control')) !!}
+                            <div class="form__error">{{ $errors->first('sections.' . $section->id . '.data.0.description') }}</div>
+                        </fieldset>
+                    </div>
 
-                <div class="form-group">
-                    <fieldset class="{{ $errors->has('sections.' . $section->id . '.data.0.unit') ? 'error' : '' }}">
-                        <label for="highlight-unit-{{ $section->id }}">Unit</label>
-                        {!! Form::text( 'sections[' . $section->id . '][data][0][unit]', $section->data[0]['unit'], array('id' => 'highlight-unit-' . $section->id, 'class' => 'form-control')) !!}
-                        <div class="form__error">{{ $errors->first('sections.' . $section->id . '.data.0.unit') }}</div>
-                    </fieldset>
-                </div>
+                    <!-- Value -->
+                    <div class="form-group">
+                        <fieldset class="{{ $errors->has('sections.' . $section->id . '.data.0.value') ? 'error' : '' }}">
+                            <label for="highlight-value-{{ $section->id }}">Value</label>
+                            {!! Form::text( 'sections[' . $section->id . '][data][0][value]', $section->data[0]['value'], array('id' => 'highlight-value-' . $section->id, 'class' => 'form-control')) !!}
+                            <div class="form__error">{{ $errors->first('sections.' . $section->id . '.data.0.value') }}</div>
+                        </fieldset>
+                    </div>
 
-                <div class="form-group">
-                    <fieldset class="{{ $errors->has('sections.' . $section->id . '.data.0.icon') ? 'error' : '' }}">
-                        <label for="highlight-icon-{{ $section->id }}">Icon</label>
-                        {!! Form::text( 'sections[' . $section->id . '][data][0][icon]', $section->data[0]['icon'], array('id' => 'highlight-icon-' . $section->id, 'class' => 'form-control')) !!}
-                        <div class="form__error">{{ $errors->first('sections.' . $section->id . '.data.0.icon') }}</div>
-                        <span id="" class="help-block">
-                            The name of the font-awesome icon that should show for this highlight, e.g. 'ticket'.
-                        </span>
-                    </fieldset>
+                    <!-- Unit -->
+                    <div class="form-group">
+                        <fieldset class="{{ $errors->has('sections.' . $section->id . '.data.0.unit') ? 'error' : '' }}">
+                            <label for="highlight-unit-{{ $section->id }}">Unit</label>
+                            {!! Form::text( 'sections[' . $section->id . '][data][0][unit]', $section->data[0]['unit'], array('id' => 'highlight-unit-' . $section->id, 'class' => 'form-control')) !!}
+                            <div class="form__error">{{ $errors->first('sections.' . $section->id . '.data.0.unit') }}</div>
+                        </fieldset>
+                    </div>
+
+                    <!-- Icon -->
+                    <div class="form-group">
+                        <fieldset class="{{ $errors->has('sections.' . $section->id . '.data.0.icon') ? 'error' : '' }}">
+                            <label for="highlight-icon-{{ $section->id }}">Icon</label>
+                            {!! Form::text( 'sections[' . $section->id . '][data][0][icon]', $section->data[0]['icon'], array('id' => 'highlight-icon-' . $section->id, 'class' => 'form-control')) !!}
+                            <div class="form__error">{{ $errors->first('sections.' . $section->id . '.data.0.icon') }}</div>
+                            <span id="" class="help-block">
+                                The name of the font-awesome icon that should show for this highlight, e.g. 'ticket'.
+                            </span>
+                        </fieldset>
+                    </div>
                 </div>
             </div>
+            <!-- Repeatable Highlight block END -->
+
         </div>
-        <!-- Highlight block END -->
+
+        <button id="add-highlight" type="button" class="btn btn-default">Add highlight</button>
+
     </div>
 
-    <button type="button" class="btn btn-default">Add highlight</button>
 
-</div>
+@section('scripts')
+    <script>
+
+        console.log('Name value JS loaded');
+
+        $(document).ready(function() {
+
+            $('#add-highlight').on('click', function(e) {
+                e.preventDefault();
+
+                console.log('TODO: Add a highlight here.');
+            });
+
+        });
+
+    </script>
+@endsection
